ee305bfb561870cec23280f0f5408c389ceda7cb
homophobiaa <deo08mine@gmail.com>
2024-10-28 11:20:08 +0200

    commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Personal Branding Website
+
+This is my personal branding website, built with HTML, CSS, and JavaScript. The site features a main page, a projects showcase, a contact page, and an interactive game.
+You can visit it at [deyanilkov.com](https://deyanilkov.com) .
+
+## Features
+
+- **Mouse Follow Effects:** Inspired by [Iseyaaaaa's on CodePen](https://codepen.io/Iseyaaaaa/pen/qBMNEGN), creating an engaging user experience.
+- **Functional Contact Form:** Integrated with the Formspree API for seamless communication.
+- **Responsive Design:** Optimized for both desktop and mobile devices.
+
+Feel free to explore the site and use parts of its code! You can contact me via the contact form or one of the other methods on the website.
---
+++ javascript/script.js
@@ -0,0 +1,36 @@
+let currentSlide = 0;
+
+function changeSlide(direction) {
+    const images = document.querySelectorAll('.carousel-images img');
+    images[currentSlide].classList.remove('active'); // Hide current image
+
+    currentSlide += direction; // Move to the next or previous slide
+
+    // Loop back to the beginning or end
+    if (currentSlide >= images.length) {
+        currentSlide = 0;
+    } else if (currentSlide < 0) {
+        currentSlide = images.length - 1;
+    }
+
+    images[currentSlide].classList.add('active'); // Show new current image
+}
+
+// Optional: Auto-change slides every 3 seconds
+setInterval(() => changeSlide(1), 3000);
+
+document.querySelectorAll('.fade-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.body.classList.add('fade-out');
+      setTimeout(() => {
+        window.location.href = this.href;
+      }, 200); // Adjust time to match animation duration
+    });
+  });
+
+// fade in
+document.addEventListener('DOMContentLoaded', function () {
+  document.body.classList.add('fade-in');
+});
+
